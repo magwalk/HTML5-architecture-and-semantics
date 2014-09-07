367344d05b0245303de7340839bc6d8c22fead01
Mimi Gim <mimileegim@gmail.com>
2014-09-07 16:51:50 -0700

    add changes to use principals of HTML5 Boilerplate correctly

---
+++ mimi-gim/js/main.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+      $(document).ready(function () {
+        function onpageload() { //This function checks to see if there are valuses stored in sessionStorage
+          if (sessionStorage["yourname"]) {
+             $('#yourname').val(sessionStorage["yourname"]);
+          }
+          else {
+            alert("Enter your name and hometown!")
+          };
+          if (sessionStorage["hometown"]) {
+             $('#hometown').val(sessionStorage["hometown"]);
+          };
+        };
+        onpageload();
+        });
+      $('.stored').keyup(function () {
+      sessionStorage[$(this).attr('name')] = $(this).val();
+      });
+      $('#testForm').submit(function() {
+      alert("Your name is " + sessionStorage["yourname"] + ". Your hometown is " + sessionStorage["hometown"] + ". Thanks for playing!");
+      });
+    </script>
