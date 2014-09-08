bd3e5ee18d9cf45989c4c124a4f21307913cb325
Maggie Walker <magwalk@gmail.com>
2014-09-08 11:32:23 -0700

    Commit #2

--- mimi-gim/js/main.js
+++ mimi-gim/js/main.js
@@ -3,9 +3,6 @@
         function onpageload() { //This function checks to see if there are valuses stored in sessionStorage
           if (sessionStorage["yourname"]) {
              $('#yourname').val(sessionStorage["yourname"]);
-          }
-          else {
-            alert("Enter your name and hometown!")
           };
           if (sessionStorage["hometown"]) {
              $('#hometown').val(sessionStorage["hometown"]);
@@ -17,6 +14,11 @@
       sessionStorage[$(this).attr('name')] = $(this).val();
       });
       $('#testForm').submit(function() {
-      alert("Your name is " + sessionStorage["yourname"] + ". Your hometown is " + sessionStorage["hometown"] + ". Thanks for playing!");
+      /*alert("Your name is " + sessionStorage["yourname"] + ". Your hometown is " + sessionStorage["hometown"] + ". Thanks for playing!"); ---- This was the previous alert*/
+      var alert-string = "Your name is " + sessionStorage["yourname"] + ". Your hometown is " + sessionStorage["hometown"] + ". Thanks for playing!";
+      $('#alert').css('display','block')
+      $('#alert').innerHTML(alert-string)
+
+
       });
 
